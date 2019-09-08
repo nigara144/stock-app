6dfde2260ee99a9a6e2d675553833be7137b3c62
nigara144 <nigar.azadaliyeva@gmail.com>
2019-09-08 15:10:28 +0300

    Add getStockData function to stockAPI.js

--- stockAPI.js
+++ stockAPI.js
@@ -1,9 +1,24 @@
 const request = require('request');
 
-request('https://api.worldtradingdata.com/api/v1/stock?symbol=AAPL,MSFT,HSBA.L&api_token=pFeLPYkfvXHhfcF5YG3M6LNYueU2mlizlL2s57b3Z97SbJsifvzhQo1pcY5w', {json: true}, (err, res, body) => {
-    if(err){
-        console.log(err);
-    }else{
-        console.log(body);
-    }
-});
+var stockSymbols = ["AAPL", "MSFT","HSBA.L"];
+var str = "";
+ 
+function getStockData(stockSymbolsArray){
+    stockSymbols.forEach(Symbol => {
+        str += Symbol + ",";
+    });
+    var newStr = str.slice(0, str.length-1);
+
+    request('https://api.worldtradingdata.com/api/v1/stock?symbol='+ newStr +'&api_token=pFeLPYkfvXHhfcF5YG3M6LNYueU2mlizlL2s57b3Z97SbJsifvzhQo1pcY5w', {json: true}, (err, res, body) => {
+        if(err){
+            console.log(err);
+        }else{
+            console.log(body);
+        }
+    });
+}
+
+getStockData(stockSymbols);
+
+
+

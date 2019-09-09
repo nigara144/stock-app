22d3f02640c24efc84209b37eaa9faff31f77c2d
nigara144 <nigar.azadaliyeva@gmail.com>
2019-09-09 17:35:30 +0300

    Change getStockData function in stockAPI.js

--- stockAPI.js
+++ stockAPI.js
@@ -12,13 +12,16 @@ function getStockData(stockSymbolsArray){
     request('https://api.worldtradingdata.com/api/v1/stock?symbol='+ newStr +'&api_token=pFeLPYkfvXHhfcF5YG3M6LNYueU2mlizlL2s57b3Z97SbJsifvzhQo1pcY5w', {json: true}, (err, res, body) => {
         if(err){
             console.log(err);
-        }else{
-            return body;
+        }else{ 
+            console.log(res.body);
+            return res.body;
         }
     });
 }
 
 getStockData(stockSymbols);
+
+
 
 
 

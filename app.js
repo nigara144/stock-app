acfdd434a8d21e5db60ea76a216cdc5c27533637
nigara144 <nigar.azadaliyeva@gmail.com>
2019-09-10 19:31:42 +0300

    add portfolio and login routes to app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-var express = require("express");
-var app     = express();
-var bodyParser = require("body-parser");
-var path = require('path');
+var express = require("express"),
+    app     = express(),
+	bodyParser = require("body-parser");
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -9,6 +8,20 @@ app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
 	res.render("landing");
+});
+
+app.get("/portfolio", function(req,res){
+	res.render("portfolio");
+});
+
+//show login form
+app.get("/login", function(req,res){
+	res.render("login");
+});
+
+//handle login logic
+app.post("/login",function(req, res){
+	res.redirect("/portfolio");
 });
 
 app.listen(process.argv[2], function(){
